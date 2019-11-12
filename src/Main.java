5f17c9c0bcd87208e221735cb09b5c901e3d1842
EthanPugh <ethanthomaspugh@hotmail.com>
2019-11-12 19:25:59 +0000

    Formatting adjustments.

--- src/Main.java
+++ src/Main.java
@@ -22,30 +22,30 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	private static final String FRAME_TITLE = "Shape Booooiiinggg Frame";
+    private static final String FRAME_TITLE = "Shape Booooiiinggg Frame";
 
 	public static void main(String[] args) {
-		launch(args);
-	}
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) {
+    @Override
+    public void start(Stage primaryStage) {
 
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter a shape file to open:");
-		String filename = input.next();
-		input.close();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a shape file to open:");
+        String filename = input.next();
+        input.close();
 
-		primaryStage.setTitle(FRAME_TITLE);
-		Group root = new Group();
-		Canvas canvas = new Canvas(800, 800);
-		GraphicsContext gc = canvas.getGraphicsContext2D();
+        primaryStage.setTitle(FRAME_TITLE);
+        Group root = new Group();
+        Canvas canvas = new Canvas(800, 800);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-		new BouncingShapesWindow(gc, filename);
-		root.getChildren().add(canvas);
-		primaryStage.setScene(new Scene(root));
-		primaryStage.show();
+        new BouncingShapesWindow(gc, filename);
+        root.getChildren().add(canvas);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
 
-	}
+    }
 
 }
